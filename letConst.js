84814d5c7c1293d21378500b5bb8cf7ccab2836f
miftajannat <miftajannat.dev@gmail.com>
2022-10-23 11:20:21 +0600

    second commit

---
+++ letConst.js
@@ -0,0 +1,14 @@
+// value can't be changed
+const hubby = "Buffy Khan";
+console.log(hubby);
+
+// elements of an array can be changed
+const numbers = [12, 45];
+numbers[1] = 88;
+console.log(numbers);
+
+//push can be used to add elements in an array
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+console.log(numbers);
